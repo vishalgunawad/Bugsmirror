d11e8c1e5b915a3ddb3259ec76f1072afc3e9af3
vishalgunawad <vishalgunawad@gmail.com>
2022-12-21 01:44:58 +0530

    FinalCommit

---
+++ README.txt
@@ -0,0 +1,55 @@
+
+This is a Backend API made with Golang and Mongodb (used for User data storing)
+
+to run this project go to the main directory insie the terminal and type "go run main.go"
+it will start the server 
+
+
+now use TunderClient OR Postman or any api calling software for  calling API
+
+i am using TunderClient below is the examples for calling api in TunderClient
+
+
+// CREATE USER
+
+URL  = localhost:8080/api/v1/Users
+MEATHOD = POST
+Http Headers = "Content-Type: application/json" 
+Body ={
+    "id": 1,
+    "First_Name": "Leanne Graham",
+    "Last_Name": "Bret",
+    "Date_Of_Birth" : "01/01/2000",
+    "Email": "Sincere@april.biz",
+    "Phone_number" : 123456789
+  }
+
+
+//GET ALL USER
+
+URL  = localhost:8080/api/v1/Users
+MEATHOD = GET
+Http Headers = "Content-Type: application/json" 
+
+
+
+//EDIT USER     {it will edit the user which have the id matching in the database}
+
+URL  = localhost:8080/api/v1/Users
+MEATHOD = PUT
+Http Headers = "Content-Type: application/json" 
+Body ={
+    "id": 1,
+    "First_Name": "Leanne Graham",
+    "Last_Name": "Bret",
+    "Date_Of_Birth" : "01/01/2000",
+    "Email": "Sincere@april.biz",
+    "Phone_number" : 123456789
+  }
+
+// DELETE USER
+
+URL  = localhost:8080/api/v1/Users
+MEATHOD = DELETE
+Http Headers = "Content-Type: application/json" 
+Body = { "id": 1}
--- main.go
+++ main.go
@@ -2,33 +2,26 @@ package main
 
 import (
 	"context"
-
-	"net/http"
-
 	"encoding/json"
-
-	_ "log"
-
 	"fmt"
-
-	"go.mongodb.org/mongo-driver/mongo"
-
 	"go.mongodb.org/mongo-driver/bson"
-
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	_ "log"
+	"net/http"
 )
 
 const (
-	dbUser = "mongo_db_admin"
+	dbUser = "dark"
 
-	dbPass = "EXAMPLE_PASSWORD"
+	dbPass = "lordvishal"
 
-	dbName = "shop_db"
+	dbName = "BugsMirror"
 )
 
 func main() {
 
-	http.HandleFunc("/api/v1/products", requestHandler)
+	http.HandleFunc("/api/v1/Users", requestHandler)
 
 	http.ListenAndServe(":8080", nil)
 
@@ -42,7 +35,8 @@ func requestHandler(w http.ResponseWriter, req *http.Request) {
 
 	ctx := context.Background()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+dbUser+":"+dbPass+"@localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://dark:lordvishal@cluster0.pppaw.mongodb.net/BugsMirror"))
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://dark:lordvishal@cluster0.pppaw.mongodb.net/BugsMirror"))
 
 	if err != nil {
 
@@ -50,7 +44,7 @@ func requestHandler(w http.ResponseWriter, req *http.Request) {
 
 	}
 
-	collection := client.Database(dbName).Collection("products")
+	collection := client.Database(dbName).Collection("Users")
 
 	data := map[string]interface{}{}
 
@@ -66,19 +60,19 @@ func requestHandler(w http.ResponseWriter, req *http.Request) {
 
 	case "POST":
 
-		response, err = createRecord(collection, ctx, data)
+		response, err = Create_a_new_User(collection, ctx, data)
 
 	case "GET":
 
-		response, err = getRecords(collection, ctx)
+		response, err = Get_All_Users(collection, ctx)
 
 	case "PUT":
 
-		response, err = updateRecord(collection, ctx, data)
+		response, err = Edit_User(collection, ctx, data)
 
 	case "DELETE":
 
-		response, err = deleteRecord(collection, ctx, data)
+		response, err = Delete_a_User(collection, ctx, data)
 
 	}
 
@@ -99,65 +93,8 @@ func requestHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 }
-func deleteRecord(collection *mongo.Collection, ctx context.Context, data map[string]interface{}) (map[string]interface{}, error) {
 
-	_, err := collection.DeleteOne(ctx, bson.M{"product_id": data["product_id"]})
-
-	if err != nil {
-
-		return nil, err
-
-	}
-
-	res := map[string]interface{}{
-
-		"data": "Document deleted successfully.",
-	}
-
-	return res, nil
-
-}
-
-func getRecords(collection *mongo.Collection, ctx context.Context) (map[string]interface{}, error) {
-
-	cur, err := collection.Find(ctx, bson.D{})
-
-	if err != nil {
-
-		return nil, err
-
-	}
-
-	defer cur.Close(ctx)
-
-	var products []bson.M
-
-	for cur.Next(ctx) {
-
-		var product bson.M
-
-		if err = cur.Decode(&product); err != nil {
-
-			return nil, err
-
-		}
-
-		products = append(products, product)
-
-	}
-
-	res := map[string]interface{}{}
-
-	res = map[string]interface{}{
-
-		"data": products,
-	}
-
-	return res, nil
-
-}
-
-func createRecord(collection *mongo.Collection, ctx context.Context, data map[string]interface{}) (map[string]interface{}, error) {
+func Create_a_new_User(collection *mongo.Collection, ctx context.Context, data map[string]interface{}) (map[string]interface{}, error) {
 
 	req, err := collection.InsertOne(ctx, data)
 
@@ -181,9 +118,67 @@ func createRecord(collection *mongo.Collection, ctx context.Context, data map[st
 
 }
 
-func updateRecord(collection *mongo.Collection, ctx context.Context, data map[string]interface{}) (map[string]interface{}, error) {
+func Delete_a_User(collection *mongo.Collection, ctx context.Context, data map[string]interface{}) (map[string]interface{}, error) {
 
-	filter := bson.M{"product_id": data["product_id"]}
+	_, err := collection.DeleteOne(ctx, bson.M{"id": data["id"]})
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	res := map[string]interface{}{
+
+		"data": "User deleted successfully.",
+	}
+
+	return res, nil
+
+}
+
+func Get_All_Users(collection *mongo.Collection, ctx context.Context) (map[string]interface{}, error) {
+
+	cur, err := collection.Find(ctx, bson.D{})
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	defer cur.Close(ctx)
+
+	var Users []bson.M
+
+	for cur.Next(ctx) {
+
+		var product bson.M
+
+		if err = cur.Decode(&product); err != nil {
+
+			return nil, err
+
+		}
+
+		Users = append(Users, product)
+
+	}
+
+	res := map[string]interface{}{}
+
+	res = map[string]interface{}{
+
+		"data": Users,
+	}
+
+	return res, nil
+
+}
+
+func Edit_User(collection *mongo.Collection, ctx context.Context, data map[string]interface{}) (map[string]interface{}, error) {
+
+	filter := bson.M{"id": data["id"]}
 
 	fields := bson.M{"$set": data}
 
@@ -197,7 +192,7 @@ func updateRecord(collection *mongo.Collection, ctx context.Context, data map[st
 
 	res := map[string]interface{}{
 
-		"data": "Document updated successfully.",
+		"data": "Users Edited successfully.",
 	}
 
 	return res, nil
